8cc2b32ae80476fc59a8843804729da6faa03a5e
Myriam_Braconier <151550408+volubyl01@users.noreply.github.com>
2024-10-17 17:02:49 +0200

    Update app.js
    correction syntaxe coreoptions

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const cors = require('cors');
 const corsOptions = {
   origin: [
   'http://localhost:3000', 
-  'https://volubyl01.github.io', 
+  'https://volubyl01.github.io'
+  ], 
   optionsSuccessStatus: 200,
 };
 
