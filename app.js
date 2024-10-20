26bb69729e6fb54bf580806e252d61dad59f153a
Myriam_Braconier <151550408+volubyl01@users.noreply.github.com>
2024-10-20 12:34:06 +0200

    Update app.js
    correction de la structure du fichier app.js

--- app.js
+++ app.js
@@ -4,21 +4,18 @@ const jokeRoutes = require("./routes/jokeRoutes");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpecs = require("./swagger");
 const cors = require("cors");
+const seedJokes = require("./seedJokes");
 
+// Initialisation de l'app
 const app = express();
 
-// Configuration fr CORS pour autoriser les requêtes de l'application React
+// Configuration CORS pour autoriser les requêtes de l'application React
 const corsOptions = {
-	origin: ["http://localhost:3000", "http://localhost:3001","https://volubyl01.github.io"],
-	optionsSuccessStatus: 200,
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://volubyl01.github.io/carambar-page"],
+    optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 app.use(express.json());
-const seedJokes = require("./seedJokes");
-seedJokes().catch((err) => {
-	console.error("Erreur lors du seeding:", err);
-	process.exit(1); // Arrêtez le processus en cas d'erreur
-});
 
 // Routes de l'API
 app.use("/api/v1/jokes", jokeRoutes);
@@ -26,29 +23,28 @@ app.use("/api/v1/jokes", jokeRoutes);
 // Documentation Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
-// Synchronisation de la base de données
-sequelize.sync()
-  .then(async () => {
-    console.log("Base de données synchronisée");
-    
-    // Effectuez le seeding après la synchronisation
-    try {
-      await seedJokes();
-    } catch (err) {
-      console.error("Erreur lors du seeding:", err);
-    }
-
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
-  })
-  .catch(err => console.error('Erreur de synchronisation:', err));
-
-
 // Middleware de gestion des erreurs
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res
-		.status(500)
-		.json({ message: "Une erreur est survenue", error: err.message });
+    console.error(err.stack);
+    res.status(500).json({ message: "Une erreur est survenue", error: err.message });
 });
 
+// Synchronisation de la base de données et démarrage du serveur
+sequelize.sync()
+    .then(async () => {
+        console.log("Base de données synchronisée");
+        
+        try {
+            await seedJokes();
+            console.log("Seeding des blagues effectué avec succès");
+        } catch (err) {
+            console.error("Erreur lors du seeding:", err);
+        }
+
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
+    })
+    .catch(err => {
+        console.error('Erreur de synchronisation ou de seeding:', err);
+        process.exit(1);
+    });
